ef84baa87fef9c894db9e2bda49d950cf42887ee
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-02-20 00:08:18 +0100

    feat: added `setCurrentGame` and `setLastGame` in `useGameStore`

--- src/stores/game.ts
+++ src/stores/game.ts
@@ -5,10 +5,12 @@ import axios from "axios";
 
 export const useGameStore = defineStore('game', () => {
   let currentGame = ref<Game | null>(null)
+  // TODO: crear un archivo de configuracion global para cargar las env variables
+  const serverUrl: string = import.meta.env.VITE_SERVER_URL
 
   const fetchLastGame = () => {
     axios.
-      get(import.meta.env.VITE_SERVER_URL + "/game/last")
+      get(serverUrl + "/game/last")
       .then(res => {
         currentGame.value = res.data
         console.log(currentGame.value)
@@ -18,8 +20,25 @@ export const useGameStore = defineStore('game', () => {
       })
   }
 
+  const setCurrentGame = (game: Game) => {
+    currentGame.value = game
+  }
+
+  const setLastGame = (): void => {
+    axios.
+      get(serverUrl + "/game/last")
+      .then(res => {
+        setCurrentGame(res.data as Game)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
   return {
     currentGame,
     fetchLastGame,
+    setCurrentGame,
+    setLastGame,
   }
 })
