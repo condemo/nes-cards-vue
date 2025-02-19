aa328b9ff23406df9a17ccc7ae20cfaab52cfb81
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-02-20 00:07:18 +0100

    feat: set `currentGame` after data fetch

--- src/composables/useCreateGame.ts
+++ src/composables/useCreateGame.ts
@@ -1,11 +1,14 @@
-import type { GameSetup } from '@/types/game'
+import { useGameStore } from '@/stores/game'
+import type { Game, GameSetup } from '@/types/game'
 import axios from 'axios'
 
+
 export const useCreateGame = (game: GameSetup): void => {
+  const gameStore = useGameStore()
   axios.
     post(import.meta.env.VITE_SERVER_URL + "/game/", game)
     .then(res => {
-      /* TODO: cambiar de vista a `game` pasandole el game actualizado */
+      gameStore.setCurrentGame(res.data as Game)
     })
     .catch(e => {
       console.log(e as Error)
