a86538415d404ae4ba39f049c4dedec8a18d7a22
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-02-21 00:44:22 +0100

    feat: basic `GameView` setup

---
+++ src/components/PlayerStat.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex flex-col space-y-3">
+    <div class="stats shadow w-40">
+      <div class="stat">
+        <div class="stat-title text-black text-xl font-bold">{{ player?.name }}</div>
+        <div class="stat-value text-primary">{{ playerStats?.hp }}</div>
+      </div>
+    </div>
+    <div class="divider"></div>
+    <TowerStat :thp="playerStats?.t1hp" />
+    <TowerStat :thp="playerStats?.t2hp" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { PropType } from 'vue';
+import type { Player, PlayerStats } from '@/types/game';
+import TowerStat from './TowerStat.vue'
+
+defineProps({
+  player: {
+    type: Object as PropType<Player>,
+  },
+  playerStats: {
+    type: Object as PropType<PlayerStats>
+  }
+})
+</script>
--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,11 +1,10 @@
 <template>
   <div>
-    <h1 class="text-4xl font-bold m-1">GameView</h1>
     <Loading v-if="loading" />
-    <div v-else-if="currentGame" id="current-game" class="flex flex-col space-y-2">
-      <div id="players-stats" class="flex flex-row">
-        <p>{{ currentGame?.player1.name }}</p>
-        <p>{{ currentGame?.player2.name }}</p>
+    <div v-else-if="currentGame" id="current-game" class="flex flex-col mx-1">
+      <div id="players-stats" class="flex flex-row justify-evenly m-2 w-full p-1 mx-auto">
+        <PlayerStat :player="currentGame.player1" :player-stats="currentGame.p1stats" />
+        <PlayerStat :player="currentGame.player2" :player-stats="currentGame.p2stats" />
       </div>
     </div>
     <div v-else id="empty-view">
@@ -19,6 +18,7 @@
 import { useGameStore } from '@/stores/game';
 import { storeToRefs } from 'pinia';
 import Loading from '@/components/Loading.vue';
+import PlayerStat from '@/components/PlayerStat.vue'
 
 const gameStore = useGameStore()
 const { currentGame, loading } = storeToRefs(gameStore)
