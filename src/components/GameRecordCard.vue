fc4615fa139ec1be0a69f3c72904c8a34184d72c
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-02-21 22:38:44 +0100

    feat(ui): added a loading state and spinner

--- src/components/GameRecordCard.vue
+++ src/components/GameRecordCard.vue
@@ -1,12 +1,10 @@
 <template>
-  <div>
-    <div class="card bg-base-100 shadow-xl">
-      <div class="card-body">
-        <h2 class="card-title">{{ formatedDate }}</h2>
-        <div class="flex flex-row justify-around">
-          <SimplePlayerStat :player="game.player1" :stats="game.p1stats" :winner="game.player1.name === game.winner" />
-          <SimplePlayerStat :player="game.player2" :stats="game.p2stats" :winner="game.player2.name === game.winner" />
-        </div>
+  <div @click="setCurrentGame" class="card bg-base-100 shadow-xl cursor-pointer">
+    <div class="card-body">
+      <h2 class="card-title">{{ formatedDate }}</h2>
+      <div class="flex flex-row justify-around">
+        <SimplePlayerStat :player="game.player1" :stats="game.p1stats" :winner="game.player1.name === game.winner" />
+        <SimplePlayerStat :player="game.player2" :stats="game.p2stats" :winner="game.player2.name === game.winner" />
       </div>
     </div>
   </div>
@@ -16,6 +14,8 @@
 import type { Game } from '@/types/game';
 import type { PropType } from 'vue';
 import SimplePlayerStat from './partials/SimplePlayerStat.vue'
+import { useGameStore } from '@/stores/game';
+import { useRouter } from 'vue-router';
 
 const props = defineProps({
   game: {
@@ -27,5 +27,13 @@ const props = defineProps({
 const date = new Date((props.game?.createdAt as string))
 const m = date.getMonth() + 1
 const formatedDate = date.getDate() + "/" + m + "/" + date.getFullYear()
+
+const gameStore = useGameStore()
+const router = useRouter()
+
+const setCurrentGame = () => {
+  gameStore.setCurrentGame(props.game)
+  router.push("/game")
+}
 
 </script>
--- src/views/ConfigView.vue
+++ src/views/ConfigView.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
     <h1 class="text-4xl font-bold">Config</h1>
-    <img class="py-5" src="https://pbs.twimg.com/media/E8c6-2pWYAEAjRG?format=jpg&name=medium" alt="no lo hice">
+    <img @load="isLoaded = true" class="py-5 mx-auto"
+      src="https://pbs.twimg.com/media/E8c6-2pWYAEAjRG?format=jpg&name=medium" alt="no lo hice">
+    <Loading v-if="!isLoaded" />
   </div>
 </template>
+
+<script setup lang="ts">
+import Loading from '@/components/Loading.vue';
+import { ref } from 'vue';
+
+const isLoaded = ref<boolean>(false)
+</script>
