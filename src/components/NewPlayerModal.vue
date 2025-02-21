8b41b571650eb0ae3a791bd9b984403d1f04bd47
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-02-21 19:13:06 +0100

    feat: `NewPlayerModal`

---
+++ src/components/NewPlayerModal.vue
@@ -0,0 +1,22 @@
+<template>
+  <dialog id="new_player_modal" class="modal modal-top">
+    <div class="modal-box">
+      <div class="flex flex-col">
+        <input v-model="playerName" type="text" placeholder="Type here"
+          class="input input-bordered input-primary w-3/4 mx-auto max-w-xs my-4" />
+        <button class="btn btn-primary w-1/3 mx-auto">Create</button>
+      </div>
+      <div class="modal-action">
+        <form method="dialog">
+          <button class="btn fixed top-1 right-1">X</button>
+        </form>
+      </div>
+    </div>
+  </dialog>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const playerName = ref<string>('')
+</script>
--- src/views/NewGameView.vue
+++ src/views/NewGameView.vue
@@ -15,6 +15,14 @@
             <option v-for="p in playerList" :value="p" :key="p.id" :selected="p.name === 'Player1' ? true : false">{{
               p.name }}</option>
           </select>
+          <button onclick="new_player_modal.showModal()" class="btn btn-primary" type="button">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+              stroke="currentColor" class="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round"
+                d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z">
+              </path>
+            </svg>
+          </button>
           <select v-model="game.player2" class="select select-primary w-2/5 max-w-xs">
             <option v-for="p in playerList" :value="p" :key="p.id" :selected="p.name === 'Player2' ? true : false">{{
               p.name }}
@@ -39,7 +47,7 @@
                 d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M9 6.75h1.5m-1.5 3h1.5m-1.5 3h1.5m3-6H15m-1.5 3H15m-1.5 3H15M9 21v-3.375c0-.621.504-1.125 1.125-1.125h3.75c.621 0 1.125.504 1.125 1.125V21">
               </path>
             </svg>
-            <input v-model.number="game.towerHP" autocomplete="off" type="number" class="grow" name="tower-hp"
+            <input v-model.nu?mber="game.towerHP" autocomplete="off" type="number" class="grow" name="tower-hp"
               placeholder="Towers HP (60) {0..255}" />
           </label>
         </div>
@@ -49,14 +57,6 @@
         :disabled="!game.player1 || !game.player2 || game.player1.name === game.player2.name">Play!</button>
     </div>
     <div class="divider"></div>
-    <button class="btn btn-primary fixed right-2 bottom-20" type="button">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-        class="size-6">
-        <path stroke-linecap="round" stroke-linejoin="round"
-          d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z">
-        </path>
-      </svg>
-    </button>
     <div class="my-2">
       <h2 class="text-2xl">Current Setup</h2>
       <div class="mx-auto">
@@ -65,6 +65,7 @@
       <p>Player HP: {{ game.playerHP }} (80)</p>
       <p>Tower HP: {{ game.towerHP }} (60)</p>
     </div>
+    <NewPlayerModal />
   </div>
 </template>
 
@@ -74,9 +75,9 @@ import type { GameSetup, Player } from '@/types/game'
 import { useFetch } from '@/composables/useFetch';
 import { useCreateGame } from '@/composables/useCreateGame';
 import { useRouter } from 'vue-router';
+import NewPlayerModal from '@/components/NewPlayerModal.vue'
 
 const router = useRouter()
-
 const { data: playerList } = useFetch<Player[]>("/player/")
 
 const game = reactive<GameSetup>({
