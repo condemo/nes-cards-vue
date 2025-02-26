9a0dc94ae46cb7e0cbf8d60e83aaffe4d4aa70a1
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-02-27 00:57:40 +0100

    `Error.vue` -> `ErrorMessage.vue`

--- src/views/RecordView.vue
+++ src/views/RecordView.vue
@@ -3,7 +3,7 @@
     <div class="flex flex-col space-y-3">
       <h1 class="text-4xl font-bold">Records</h1>
       <LoadingSpinner v-if="loading" />
-      <Error :error="error" v-else-if="error" />
+      <ErrorMessage :error="error" v-else-if="error" />
       <GameRecordCard v-else-if="data" v-for="g in data" :key="g.id" :game="g" />
       <div id="empty-history" v-else>
         <img src="/img/why_is_empty.png" class="w-56 mx-auto" />
@@ -18,7 +18,7 @@ import { useFetch } from '@/composables/useFetch'
 import type { Game } from '@/types/game'
 import GameRecordCard from '@/components/GameRecordCard.vue'
 import LoadingSpinner from '@/components/LoadingSpinner.vue'
-import Error from '@/components/Error.vue'
+import ErrorMessage from '@/components/ErrorMessage.vue'
 
 const { data, error, loading } = useFetch<Game[]>('/game/')
 </script>
