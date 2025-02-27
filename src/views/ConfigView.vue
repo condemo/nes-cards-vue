eee7784c77bfb3ffe641fa731d62a513db8dfac9
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-02-27 18:51:32 +0100

    fix: min fix

--- src/views/ConfigView.vue
+++ src/views/ConfigView.vue
@@ -4,7 +4,7 @@
     <img @load="isLoaded = true" class="py-5 mx-auto"
       src="https://pbs.twimg.com/media/E8c6-2pWYAEAjRG?format=jpg&name=medium" alt="no lo hice" />
     <LoadingSpinner v-if="!isLoaded" />
-  </div>@/components/LoadingSpinner.vue
+  </div>
 </template>
 
 <script setup lang="ts">
