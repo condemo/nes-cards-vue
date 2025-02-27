5995677e5f225953f64b0c0d755fb66c65fde1f7
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-02-27 23:53:43 +0100

    feat: basic `GameUpdateForm`

---
+++ src/components/game/GameUpdateModal.vue
@@ -0,0 +1,18 @@
+<template>
+  <dialog id="game-update-modal" class="modal modal-bottom modal-open">
+    <div class="modal-box">
+      <button @click="$emit('close-update-modal')"
+        class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+      <GameUpdateForm :title="currentSection"></GameUpdateForm>
+    </div>
+  </dialog>
+</template>
+
+<script setup lang="ts">
+import GameUpdateForm from './forms/GameUpdateForm.vue';
+
+defineProps({
+  currentSection: { type: String, required: true }
+})
+
+</script>
---
+++ src/components/game/forms/GameUpdateForm.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h3 class="text-3xl font-bold">{{ title }}</h3>
+    <p>content</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  title: { type: String, required: true }
+})
+</script>
