77ce843759629e1a33f4302a1d259ab5299b2bb6
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-02-27 23:02:13 +0100

    feat: `UpdateMenu` when user clicks player or tower

--- src/components/PlayerStat.vue
+++ src/components/PlayerStat.vue
@@ -1,23 +1,18 @@
 <template>
   <div class="flex flex-col space-y-3">
     <div class="stats shadow w-40">
-      <div class="stat">
+      <div @click="playerMenu = !playerMenu" class="stat">
         <div class="stat-title text-black text-xl font-bold">{{ player?.name }}</div>
         <div class="flex flex-row justify-items-center text-primary mx-auto">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            class="w-8"
-          >
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-8">
             <path
-              d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z"
-            />
+              d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
           </svg>
           <p class="font-bold text-3xl">{{ playerStats?.hp }}</p>
         </div>
       </div>
     </div>
+    <UpdateMenu v-show="playerMenu" :player="true" />
     <div class="divider"></div>
     <TowerStat :thp="playerStats?.t1hp" />
     <TowerStat :thp="playerStats?.t2hp" />
@@ -25,9 +20,10 @@
 </template>
 
 <script setup lang="ts">
-import type { PropType } from 'vue'
+import { ref, type PropType } from 'vue'
 import type { Player, PlayerStats } from '@/types/game'
 import TowerStat from './TowerStat.vue'
+import UpdateMenu from './game/UpdateMenu.vue';
 
 defineProps({
   player: {
@@ -37,4 +33,6 @@ defineProps({
     type: Object as PropType<PlayerStats>,
   },
 })
+
+const playerMenu = ref<boolean>(false)
 </script>
---
+++ src/components/game/UpdateMenu.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <ul class="menu menu-horizontal bg-base-200 rounded-box">
+      <li>
+        <a class="tooltip tooltip-bottom" data-tip="Damage">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+            <path fill-rule="evenodd"
+              d="M12.963 2.286a.75.75 0 0 0-1.071-.136 9.742 9.742 0 0 0-3.539 6.176 7.547 7.547 0 0 1-1.705-1.715.75.75 0 0 0-1.152-.082A9 9 0 1 0 15.68 4.534a7.46 7.46 0 0 1-2.717-2.248ZM15.75 14.25a3.75 3.75 0 1 1-7.313-1.172c.628.465 1.35.81 2.133 1a5.99 5.99 0 0 1 1.925-3.546 3.75 3.75 0 0 1 3.255 3.718Z"
+              clip-rule="evenodd" />
+          </svg>
+        </a>
+      </li>
+      <li v-if="player">
+        <a class="tooltip tooltip-bottom" data-tip="Altered States">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+            <path fill-rule="evenodd"
+              d="M10.5 3.798v5.02a3 3 0 0 1-.879 2.121l-2.377 2.377a9.845 9.845 0 0 1 5.091 1.013 8.315 8.315 0 0 0 5.713.636l.285-.071-3.954-3.955a3 3 0 0 1-.879-2.121v-5.02a23.614 23.614 0 0 0-3 0Zm4.5.138a.75.75 0 0 0 .093-1.495A24.837 24.837 0 0 0 12 2.25a25.048 25.048 0 0 0-3.093.191A.75.75 0 0 0 9 3.936v4.882a1.5 1.5 0 0 1-.44 1.06l-6.293 6.294c-1.62 1.621-.903 4.475 1.471 4.88 2.686.46 5.447.698 8.262.698 2.816 0 5.576-.239 8.262-.697 2.373-.406 3.092-3.26 1.47-4.881L15.44 9.879A1.5 1.5 0 0 1 15 8.818V3.936Z"
+              clip-rule="evenodd" />
+          </svg>
+
+        </a>
+      </li>
+      <li>
+        <a class="tooltip tooltip-bottom" data-tip="Defense">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+            <path fill-rule="evenodd"
+              d="M11.484 2.17a.75.75 0 0 1 1.032 0 11.209 11.209 0 0 0 7.877 3.08.75.75 0 0 1 .722.515 12.74 12.74 0 0 1 .635 3.985c0 5.942-4.064 10.933-9.563 12.348a.749.749 0 0 1-.374 0C6.314 20.683 2.25 15.692 2.25 9.75c0-1.39.223-2.73.635-3.985a.75.75 0 0 1 .722-.516l.143.001c2.996 0 5.718-1.17 7.734-3.08ZM12 8.25a.75.75 0 0 1 .75.75v3.75a.75.75 0 0 1-1.5 0V9a.75.75 0 0 1 .75-.75ZM12 15a.75.75 0 0 0-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 0 0 .75-.75v-.008a.75.75 0 0 0-.75-.75H12Z"
+              clip-rule="evenodd" />
+          </svg>
+
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  player: { type: Boolean }
+})
+</script>
