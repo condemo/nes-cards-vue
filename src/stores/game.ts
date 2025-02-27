323ac3dd83e000b85822d56dafcf1a51e427a79d
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-02-27 23:54:42 +0100

    refactor: some tweaks

--- src/stores/game.ts
+++ src/stores/game.ts
@@ -6,9 +6,11 @@ import axios from 'axios'
 export const useGameStore = defineStore('game', () => {
   const currentGame = ref<Game | null>(null)
   const loading = ref<boolean>(false)
-  // TODO: crear un archivo de configuracion global para cargar las env variables
+  // TODO: crear un archivo de configuración global para cargar las env variables
   const serverUrl: string = import.meta.env.VITE_SERVER_URL
 
+  // TODO: Estas función y `setLastGame` hacen lo mismo con ligeros cambios, dejar una
+  // y crear otra función complementaria que haga la diferencia
   const fetchLastGame = () => {
     loading.value = true
     axios
@@ -23,14 +25,18 @@ export const useGameStore = defineStore('game', () => {
       .finally(() => (loading.value = false))
   }
 
+  // Set the current game from the Records (no connection with DB)
   const setCurrentGame = (game: Game) => {
     currentGame.value = game
   }
 
+  // Gets the last game and set it as current in the server
   const setLastGame = (): void => {
     loading.value = true
     axios
-      .get(serverUrl + '/game/last')
+      .get(serverUrl + '/game/last', {
+        params: { updateCurrent: true }
+      })
       .then((res) => {
         setCurrentGame(res.data as Game)
       })
@@ -39,6 +45,10 @@ export const useGameStore = defineStore('game', () => {
       })
       .finally(() => (loading.value = false))
   }
+
+  // TODO: Recibe unos nuevos stats y un id de un player para actualizarlos,
+  // envía los datos actualizados al server y si todo va bien updatea la interfaz
+  const updateStats = async () => { }
 
   return {
     currentGame,
