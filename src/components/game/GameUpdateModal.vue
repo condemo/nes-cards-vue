744654efe173b9fb416700f65342d1f82e0802e4
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-03-01 00:30:46 +0100

    feat: close modal if click outside

--- src/components/game/GameUpdateModal.vue
+++ src/components/game/GameUpdateModal.vue
@@ -1,12 +1,14 @@
 <template>
   <dialog id="game-update-modal" class="modal modal-bottom" :class="{ 'modal-open': open }">
     <div class="modal-box">
-      <button @click="$emit('close-update-modal')"
-        class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+      <button @click="close" class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
       <GameUpdateForm :title="currentSection">
         <!-- TODO: Renderizar diferentes forms dependiendo de la sección que corresponda -->
       </GameUpdateForm>
     </div>
+    <form method="dialog" class="modal-backdrop">
+      <button @click="close">close</button>
+    </form>
   </dialog>
 </template>
 
@@ -17,5 +19,10 @@ defineProps({
   currentSection: { type: String, required: true },
   open: { type: Boolean, required: true }
 })
+
+const emit = defineEmits(['close-update-modal'])
+const close = () => {
+  emit('close-update-modal')
+}
 
 </script>
