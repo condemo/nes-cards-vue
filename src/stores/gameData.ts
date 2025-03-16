24c2ce53c76f45e39b09eb471192f150c0c47a7f
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-03-16 01:55:22 +0100

    refactor: added loading state to fetch functions

--- src/stores/gameData.ts
+++ src/stores/gameData.ts
@@ -16,18 +16,22 @@ export const useGameDataStore = defineStore('gameData', () => {
   }
 
   const createPlayer = async (name: string) => {
+    loading.value = true
     const { newPlayer, error } = await useCreatePlayer(name)
     playerList.value?.push(newPlayer.value as Player)
     apiError.value = error.value
+    loading.value = false
   }
 
   const createGame = (game: GameSetup): void => {
+    loading.value = true
     const { createdGame, error } = useCreateGame(game)
     if (error.value) {
       apiError.value = error.value
     } else {
       currentGame.value = createdGame.value
     }
+    loading.value = false
   }
 
   return {
