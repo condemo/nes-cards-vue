0f0ffb6bda85a85f4999561cabcf8d7f422577d9
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-04-10 20:28:28 +0200

    refactor: removed unnecesary data to show

--- src/components/game/PlayerMoveResume.vue
+++ src/components/game/PlayerMoveResume.vue
@@ -4,11 +4,6 @@
       <p v-show="playerMove.damage.length > 0">Damage: <span v-for="(d, i) in playerMove.damage" :key="i">{{ d
           }}&nbsp;</span>
       </p>
-      <p v-show="playerMove.defense.size() > 0">Defense: <span v-for="(d, i) in defList" :key="i">{{ d.def
-          }}&nbsp;</span></p>
-      <p v-show="playerMove.strength !== 0">Strength: {{ playerMove.strength }}</p>
-      <p v-show="playerMove.intangible > 0">Intangible: {{ playerMove.intangible }}</p>
-      <p v-show="playerMove.confusion > 0">Confusion: {{ playerMove.confusion }}</p>
       <p v-show="playerMove.distract > 0">Distract: {{ playerMove.distract }}</p>
       <p v-show="playerMove.poison > 0">Poison: {{ playerMove.poison }}</p>
     </template>
@@ -17,16 +12,10 @@
 
 <script setup lang="ts">
 import { PlayerMove } from '@/game/game';
-import { computed, type PropType } from 'vue';
+import { type PropType } from 'vue';
 
-const props = defineProps({
+defineProps({
   playerMove: { type: Object as PropType<PlayerMove> }
-})
-
-const defList = computed(() => {
-  if (props.playerMove) {
-    return [...props.playerMove.defense.defenseList].reverse()
-  }
 })
 
 </script>
