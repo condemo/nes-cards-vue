0a069ac08d5cdac951c24fd4b1aaa8cad2af3e78
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-04-26 21:36:42 +0200

    refactor: moved `poison` and `distract` into `RivalForm`; `MiscForm` removed
    fix: crash `GameView`

--- src/components/game/forms/RivalForm.vue
+++ src/components/game/forms/RivalForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="stats-form" class="flex flex-col space-y-2 w-full">
+  <div id="rival-form" class="flex flex-col space-y-2 w-full">
     <div class="flex flex-row space-x-5 mx-auto text-2xl">
       <label for="st">ST:</label>
       <button class="btn btn-circle btn-secondary" @click="updateStrength(1)">+</button>
@@ -12,6 +12,18 @@
       <label @click="move.confusion = 0">{{ move.confusion }}</label>
       <button class="btn btn-circle btn-secondary" @click="updateConfusion(1)">+</button>
     </div>
+    <div class="flex flex-row space-x-5 mx-auto text-2xl">
+      <label>Distract: </label>
+      <button class="btn btn-circle btn-secondary" @click="move.distract--">-</button>
+      <label>{{ move.distract }}</label>
+      <button class="btn btn-circle btn-secondary" @click="move.distract++">+</button>
+    </div>
+    <div class="flex flex-row space-x-5 mx-auto text-2xl">
+      <label>Poison: </label>
+      <button class="btn btn-circle btn-secondary" @click="move.poison--">-</button>
+      <label>{{ move.poison }}</label>
+      <button class="btn btn-circle btn-secondary" @click="move.poison++">+</button>
+    </div>
     <div class="flex flex-row justify-around p-2 mt-2">
       <button class="btn btn-error w-1/6" @click="reset">reset</button>
       <button class="btn btn-primary w-4/6" @click="save">Save</button>
@@ -21,17 +33,20 @@
 
 <script setup lang="ts">
 import { useGameHandlerStore } from '@/stores/gameHandler';
+import { PlayerTurn } from '@/types/game';
 import { storeToRefs } from 'pinia';
 import { reactive, watch } from 'vue';
 
 const emits = defineEmits(['close-update-modal'])
 
 const gameHandlerStore = useGameHandlerStore()
-const { currentPlayerTurn, turnMode } = storeToRefs(gameHandlerStore)
+const { currentPlayerTurn, turnMode, player1Move, player2Move } = storeToRefs(gameHandlerStore)
 
 const move = reactive({
   confusion: 0,
-  strength: 0
+  strength: 0,
+  poison: 0,
+  distract: 0
 })
 
 const updateConfusion = (count: number) => {
@@ -44,9 +59,26 @@ const updateStrength = (count: number) => {
   if (move.strength > 0) { move.strength = 0 }
 }
 
+let playerMove = (
+  currentPlayerTurn.value === PlayerTurn.Player1
+    ? player1Move
+    : player2Move
+)
+
+watch(() => currentPlayerTurn.value, () => {
+  playerMove = (
+    currentPlayerTurn.value === PlayerTurn.Player1
+      ? player1Move
+      : player2Move
+  )
+  reset()
+})
+
 const reset = () => {
   move.strength = 0
   move.confusion = 0
+  move.distract = 0
+  move.poison = 0
 }
 
 
@@ -60,6 +92,8 @@ watch(() => turnMode.value, () => {
 
 const save = () => {
   gameHandlerStore.updateRivalStats(move)
+  gameHandlerStore.distractRival(move.distract)
+  playerMove.value.poison += move.poison
   emits('close-update-modal')
   reset()
 }
