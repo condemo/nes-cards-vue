18894b2d1e4b1397e62302dc493d3a7d64b9f040
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-05-05 22:11:50 +0200

    ui: min tweaks
    min tweaks

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -26,13 +26,13 @@
             </div>
           </div>
           <div class="my-2 text-neutral">
-            <p>ver.: {{ currentVersion }} build.: {{ currentBuild }}</p>
+            <p>ver_: {{ currentVersion }} build_: {{ currentBuild }}</p>
           </div>
         </div>
         <div id="web-links" class="flex flex-row justify-evenly mt-3 space-x-1 *:w-14 *:h-14">
           <a href="https://github.com/condemo" target="_blank">
             <button class="btn bg-black text-black border-[#e5e5e5] w-14 h-14">
-              <svg aria-label="GitHub logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <svg aria-label="GitHub account" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                 <path fill="white"
                   d="M12,2A10,10 0 0,0 2,12C2,16.42 4.87,20.17 8.84,21.5C9.34,21.58 9.5,21.27 9.5,21C9.5,20.77 9.5,20.14 9.5,19.31C6.73,19.91 6.14,17.97 6.14,17.97C5.68,16.81 5.03,16.5 5.03,16.5C4.12,15.88 5.1,15.9 5.1,15.9C6.1,15.97 6.63,16.93 6.63,16.93C7.5,18.45 8.97,18 9.54,17.76C9.63,17.11 9.89,16.67 10.17,16.42C7.95,16.17 5.62,15.31 5.62,11.5C5.62,10.39 6,9.5 6.65,8.79C6.55,8.54 6.2,7.5 6.75,6.15C6.75,6.15 7.59,5.88 9.5,7.17C10.29,6.95 11.15,6.84 12,6.84C12.85,6.84 13.71,6.95 14.5,7.17C16.41,5.88 17.25,6.15 17.25,6.15C17.8,7.5 17.45,8.54 17.35,8.79C18,9.5 18.38,10.39 18.38,11.5C18.38,15.32 16.04,16.16 13.81,16.41C14.17,16.72 14.5,17.33 14.5,18.26C14.5,19.6 14.5,20.68 14.5,21C14.5,21.27 14.66,21.59 15.17,21.5C19.14,20.16 22,16.42 22,12A10,10 0 0,0 12,2Z">
                 </path>
@@ -41,7 +41,7 @@
           </a>
           <a href="mailto:gustleo.dev@gmail.com">
             <button class="btn bg-white text-black border-[#e5e5e5] w-14 h-14">
-              <svg aria-label="Email icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <svg aria-label="Send Email" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                 <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="black">
                   <rect width="20" height="16" x="2" y="4" rx="2"></rect>
                   <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
