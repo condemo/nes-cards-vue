6e8418ef895810bb752bb53fa6608122bb2265d3
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-05-08 21:40:19 +0200

    fix: `WinModal` pops if there is no game

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -4,10 +4,18 @@
     <div v-else-if="currentGame" id="current-game" class="flex flex-col mx-1">
       <h1 class="text-2xl">Round: {{ roundCount }}</h1>
       <div id="players-stats" class="flex flex-row justify-evenly m-2 w-full p-1 mx-auto">
-        <PlayerStat :player-position="PlayerTurn.Player1" :player="currentGame.player1"
-          :player-stats="currentGame.p1stats" @open-update-modal="openUpdateModal" />
-        <PlayerStat :player-position="PlayerTurn.Player2" :player="currentGame.player2"
-          :player-stats="currentGame.p2stats" @open-update-modal="openUpdateModal" />
+        <PlayerStat
+          :player-position="PlayerTurn.Player1"
+          :player="currentGame.player1"
+          :player-stats="currentGame.p1stats"
+          @open-update-modal="openUpdateModal"
+        />
+        <PlayerStat
+          :player-position="PlayerTurn.Player2"
+          :player="currentGame.player2"
+          :player-stats="currentGame.p2stats"
+          @open-update-modal="openUpdateModal"
+        />
       </div>
       <button @click="nextTurn" class="btn btn-primary text-xl font-bold">Next Turn</button>
       <MoveResumeSection />
@@ -16,12 +24,25 @@
       <img src="/img/empty_meme.jpeg" class="w-56 mx-auto" />
       <h1 class="text-3xl font-bold my-2">There is no game yet</h1>
     </div>
-    <WinModal v-if="currentGame?.winner !== 'none'" :winner="currentGame?.winner" />
-    <GameUpdateModal @close-update-modal="closeUpdateModal" :current-section="updateSection" :open="updateModal">
-      <DamageForm v-if="updateSection === UpdateMode.Damage" @close-update-modal="closeUpdateModal" />
-      <StatsForm v-else-if="updateSection === UpdateMode.Stats" @close-update-modal="closeUpdateModal" />
-      <DefenseForm v-else-if="updateSection === UpdateMode.Defense" :update-menu="true"
-        @close-update-modal="closeUpdateModal" />
+    <WinModal v-if="currentGame?.winner !== 'none' && currentGame" :winner="currentGame?.winner" />
+    <GameUpdateModal
+      @close-update-modal="closeUpdateModal"
+      :current-section="updateSection"
+      :open="updateModal"
+    >
+      <DamageForm
+        v-if="updateSection === UpdateMode.Damage"
+        @close-update-modal="closeUpdateModal"
+      />
+      <StatsForm
+        v-else-if="updateSection === UpdateMode.Stats"
+        @close-update-modal="closeUpdateModal"
+      />
+      <DefenseForm
+        v-else-if="updateSection === UpdateMode.Defense"
+        :update-menu="true"
+        @close-update-modal="closeUpdateModal"
+      />
       <RivalForm v-else @close-update-modal="closeUpdateModal" />
     </GameUpdateModal>
   </div>
@@ -69,5 +90,4 @@ const openUpdateModal = (mode: UpdateMode) => {
   updateModal.value = true
   updateSection.value = mode
 }
-
 </script>
